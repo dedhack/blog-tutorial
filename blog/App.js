4252921167646f48556039ef7c4c2f50bb9668d4
izharj <eezhar@gmail.com>
2023-03-21 21:13:24 +0800

    created context provider and reducer. Add function created

--- blog/App.js
+++ blog/App.js
@@ -1,16 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { BlogProvider } from "./src/context/BlogContext";
+// screens
+import IndexScreen from "./src/screens/IndexScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Index" component={IndexScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
 
---
+++ blog/src/screens/IndexScreen.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import React, { useContext } from "react";
+import BlogContext from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(data) => data.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
